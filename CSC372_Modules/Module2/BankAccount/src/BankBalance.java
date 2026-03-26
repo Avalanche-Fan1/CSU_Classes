8a710b2c83422e7495ed65879084379c1086326b
Avalanche-Fan1 <david.gray@csuglobal.edu>
2026-03-25 21:55:22 -0600

    First Commit

---
+++ CSC372_Modules/Module2/BankAccount/src/BankBalance.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class BankBalance {
+
+    public static void main (String[] args) {
+        JFrame myFrame = new JFrame("Bank Balance App");
+        myFrame.setSize(400,300);
+        myFrame.setLocationRelativeTo(null);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
+    }
+
+}
