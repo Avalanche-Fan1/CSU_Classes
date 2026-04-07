5d50a5f59ed4da38b26a0f1ae0e4eec1017f7926
Avalanche-Fan1 <david.gray@csuglobal.edu>
2026-04-06 22:12:29 -0600

    Final

--- CSC372_Modules/Module4/ShapeApp/src/ShapeArray.java
+++ CSC372_Modules/Module4/ShapeApp/src/ShapeArray.java
@@ -6,9 +6,9 @@ public class ShapeArray {
         Shape[] shapeArray = new Shape[3];
 
         // Add one of each shape type to the array //
-        shapeArray[0] = new Sphere(4);
-        shapeArray[1] = new Cylinder(5, 6);
-        shapeArray[2] = new Cone(3, 7);
+        shapeArray[0] = new Sphere(4.0);
+        shapeArray[1] = new Cylinder(5.0, 6.0);
+        shapeArray[2] = new Cone(3.0, 7.0);
 
         // For each shape call the toString method to print out each shape //
         for (Shape shape : shapeArray) {
