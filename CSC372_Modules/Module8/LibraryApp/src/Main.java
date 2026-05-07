bc5c92f57247f25ad814e3a9e99e93689f209c79
Avalanche-Fan1 <david.gray@csuglobal.edu>
2026-05-06 20:54:21 -0600

    Final Test

--- CSC372_Modules/Module8/LibraryApp/src/Main.java
+++ CSC372_Modules/Module8/LibraryApp/src/Main.java
@@ -1,2 +1,108 @@
+import java.util.Scanner;
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        String choice = "";
+        Inventory myInventory = new Inventory();
+
+        try {
+            while (!choice.equals("6")) {
+
+
+                printMenu();
+                choice = scnr.nextLine();
+
+
+                switch (choice) {
+
+
+                    case "1":
+
+                        System.out.print("Enter Book ID: ");
+                        int id = scnr.nextInt();
+                        scnr.nextLine();
+
+                        System.out.print("Enter Book Title: ");
+                        String title = scnr.nextLine();
+
+                        System.out.print("Enter Book Author: ");
+                        String author = scnr.nextLine();
+
+                        System.out.print("Enter Book ISBN: ");
+                        String isbn = scnr.nextLine();
+
+                        System.out.print("Enter Book Pages: ");
+                        int pages = scnr.nextInt();
+                        scnr.nextLine();
+
+                        myInventory.addBook(id,title,author,isbn,pages);
+
+                        break;
+
+
+                    case "2":
+
+                        System.out.print("Enter Book ID to borrow: ");
+                        int borrowID = scnr.nextInt();
+                        scnr.nextLine();
+
+                        myInventory.borrowBook(borrowID);
+                        break;
+
+
+                    case "3":
+                        System.out.print("Enter Book ID to return: ");
+                        int returnID = scnr.nextInt();
+                        scnr.nextLine();
+
+                        myInventory.returnBook(returnID);
+                        break;
+
+                    case "4":
+                        System.out.print("Enter title to search for: ");
+                        String searchTitle = scnr.nextLine();
+
+                        myInventory.searchByTitle(searchTitle);
+                        break;
+
+                    case "5":
+
+                        myInventory.printAll();
+                        break;
+
+                    case "6":
+                        System.out.println("Exiting program.");
+                        break;
+
+
+                    default:
+                        System.out.println("Invalid choice.");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Main error: " + e.getMessage());
+        }
+
+
+        scnr.close();
+    }
+
+    public static void printMenu() {
+        try {
+            System.out.println("\nHome Library Menu");
+            System.out.println("1. Add Book");
+            System.out.println("2. Borrow Book");
+            System.out.println("3. Return Book");
+            System.out.println("4. Search by Title");
+            System.out.println("5. Print All Books");
+            System.out.println("6. Exit");
+
+            System.out.print("Enter choice: ");
+        } catch (Exception e) {
+            System.out.println("Failed to print menu: " + e.getMessage());
+        }
+    }
+
 }
---
+++ CSC372_Modules/Module8/LibraryProject/src/Book.java
@@ -0,0 +1,81 @@
+public class Book {
+    private int bookID;
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookISBN;
+    private int bookPages;
+
+    public Book () {
+        bookID = -1;
+        bookTitle = "NoName";
+        bookAuthor = "NoName";
+        bookISBN = "NoNumber";
+        bookPages = -1;
+    }
+    public Book (int bookID, String bookTitle, String bookAuthor, String bookISBN, int bookPages) {
+        this.bookID = bookID;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookISBN = bookISBN;
+        this.bookPages = bookPages;
+    }
+
+    //Getters for Books //
+    public int getBookID(){
+        return bookID;
+    }
+
+    public String getBookTitle(){
+        return bookTitle;
+    }
+
+    public String getBookAuthor(){
+        return bookAuthor;
+    }
+
+    public String getBookISBN(){
+        return bookISBN;
+    }
+
+    public int getBookPages(){
+        return bookPages;
+    }
+
+    //Setters for Books //
+    public void setBookID(int bookID){
+        this.bookID = bookID;
+    }
+
+    public void setBookTitle(String bookTitle){
+        this.bookTitle = bookTitle;
+    }
+
+    public void setBookAuthor(String bookAuthor){
+        this.bookAuthor = bookAuthor;
+    }
+
+    public void setBookISBN(String bookISBN){
+        this.bookISBN = bookISBN;
+    }
+
+    public void setBookPages(int bookPages){
+        this.bookPages = bookPages;
+    }
+
+    // Print Book information //
+    public void printBookInfo(){
+        System.out.println();
+        System.out.println("*********************************");
+        System.out.println("ID: " + bookID);
+        System.out.println("Title: " + bookTitle);
+        System.out.println("Author: " + bookAuthor);
+        System.out.println("ISBN: " + bookISBN);
+        System.out.println("Pages: " + bookPages);
+        System.out.println("*********************************");
+
+    }
+
+}
+
+
+
