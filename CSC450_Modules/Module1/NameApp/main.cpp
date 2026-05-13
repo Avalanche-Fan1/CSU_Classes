42b31b4a84f8ebc77cfcef7360b40d781440bb27
Avalanche-Fan1 <david.gray@csuglobal.edu>
2026-05-13 15:32:27 -0600

    Add new class folder

---
+++ CSC450_Modules/Module1/NameApp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "1. John" << endl;
+    cout << "2. Lennon" << endl;
+    cout << "3. 4593 Strawberry Fields Ln" << endl;
+    cout << "4. Mahopac,NY" << endl;
+    cout << "5. 10541" << endl;
+    cin.get();
+    return 0;
+}
