95459ebb0ba577b0d11a2fe98b237c91be46a397
Avalanche-Fan1 <david.gray@csuglobal.edu>
2026-05-20 22:32:48 -0600

    Add comments

--- CSC450_Modules/Module2/main.cpp
+++ CSC450_Modules/Module2/main.cpp
@@ -1,20 +1,34 @@
+/*==============================================================================================
+== Name        : main.cpp
+== Author      : Dave Gray
+== Version     : v1
+== Description : Simple C++ Program to ask for 2 strings from user and concatenate them. Ask
+==               3 times from user (in a loop) to test varying lenghts.
+== Notable     : Used std::string for safer string handling instead of C-style character arrays
+==               This prevents buffer overflow. Out of bounds.  strcat is not safe.
+================================================================================================
+*/
+
 #include <iostream>
 #include <string>
-#include <cstring>
 using namespace std;
 
 int main() {
     string string1, string2, result;
-
+    // loop to ask user 3 times for 2 strings. //
     for (int i = 0; i < 3; i++) {
+        // ask for first string //
         cout << "Enter First String: ";
         getline(cin, string1);
 
+        // ask for second string //
         cout << "Enter Second String: ";
         getline(cin, string2);
 
+        // concatenate the strings into a new string //
         result = string1 + string2;
 
+        // print concatenated string //
         cout << result << endl;
     }
     return 0;
